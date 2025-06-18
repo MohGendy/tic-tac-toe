771b706684e4339fa158feea6c30a2960f74cd4f
MohGendy <midoomidoooo12@gmail.com>
2025-06-18 20:49:32 +0300

    bcrypt packaging

--- build/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: E:/uni/sec/term2/Embedded/project/DrOmar/code/googletest
-# Build directory: E:/uni/sec/term2/Embedded/project/DrOmar/code/build/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
