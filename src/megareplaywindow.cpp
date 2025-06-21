ccc32fc0ccf462e7fe45522bd6b658df6cbff995
MohGendy <midoomidoooo12@gmail.com>
2025-06-21 13:07:36 +0300

    minor changes

--- src/megareplaywindow.cpp
+++ src/megareplaywindow.cpp
@@ -61,13 +61,13 @@ void MainWindow::megaReplayControl(bool action,bool init){
     }else{
         if(action){
             if(i<replayer.moves.size()){
-                btns[replayer.moves[i].row*3+replayer.moves[i].col]->setText(QString(replayer.moves[i].player));
+                btns[replayer.moves[i].row*9+replayer.moves[i].col]->setText(QString(replayer.moves[i].player));
                 i++;
             }
         }else{
             if(i>0){
                 i--;
-                btns[replayer.moves[i].row*3+replayer.moves[i].col]->setText(QString(" "));
+                btns[replayer.moves[i].row*9+replayer.moves[i].col]->setText(QString(" "));
             }
         }
     }
@@ -78,13 +78,14 @@ void MainWindow::megaReplayControl(bool action,bool init){
 
 void MainWindow::on_previouspushbuttonmega_clicked()
 {
-    megaReplayControl(1,0);
+        megaReplayControl(0,0);
+
 }
 
 
 void MainWindow::on_pushButton_back_from_board_to_main_mega_r_clicked()
 {
-    megaReplayControl(0,0);
+        megaReplayControl(1,0);
 
 }
 
