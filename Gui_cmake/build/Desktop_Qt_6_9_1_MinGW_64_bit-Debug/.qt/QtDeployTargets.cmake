eea67d330e77943ae799d0ed0d587188ea028805
MohGendy <midoomidoooo12@gmail.com>
2025-06-25 12:08:48 +0300

    remove unwanted files

--- Gui_cmake/build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Gui_cmake_FILE E:/uni/sec/term2/Embedded/project/DrOmar/code/Gui_cmake/build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/Gui_cmake.exe)
-set(__QT_DEPLOY_TARGET_Gui_cmake_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Gui_cmake_RUNTIME_DLLS F:/programs/QT/6.9.1/mingw_64/bin/Qt6Widgets.dll;F:/programs/QT/6.9.1/mingw_64/bin/Qt6Gui.dll;F:/programs/QT/6.9.1/mingw_64/bin/Qt6Core.dll)
